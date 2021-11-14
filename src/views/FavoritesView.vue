fbd03964c10d670d288f17954cc54085a792c0ce
Aleksandr <a.komzoltsev@gmail.com>
2021-11-14 18:47:43 +0300

    Страницы для роутинга

---
+++ src/views/FavoritesView.vue
@@ -0,0 +1,17 @@
+<template>
+  <CardList :products="favoriteProducts" />
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+import CardList from "../components/CardList";
+import { GET_FAVORITE_PRODUCTS } from "../consts";
+
+export default {
+  name: "FavoritesView",
+  components: { CardList },
+  computed: {
+    ...mapGetters({ favoriteProducts: GET_FAVORITE_PRODUCTS }),
+  },
+};
+</script>
---
+++ src/views/ProductsView.vue
@@ -0,0 +1,16 @@
+<template>
+  <CardList :products="products" />
+</template>
+
+<script>
+import { mapState } from "vuex";
+import CardList from "../components/CardList";
+
+export default {
+  name: "ProductsView",
+  components: { CardList },
+  computed: {
+    ...mapState({ products: (state) => state.products.products }),
+  },
+};
+</script>
